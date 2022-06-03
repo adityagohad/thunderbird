04d4ef24cf6ae04b1c13858c579861b2d0f61cdb
Aditya Gohad <adityagohad@gmail.com>
2022-06-03 11:07:40 +0530

    new data

--- scripts/pp.js
+++ scripts/pp.js
@@ -620,6 +620,452 @@ var x =   [
         "7"
       ]
     }
+  },
+  {
+    "slug": "support-taken-l0jz4yv7",
+    "title": "Support Taken",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/b91b12e0-9fe1-11ec-a634-2f1716dd45af-Take-support.png",
+        "imgix_url": "https://imgix.cosmicjs.com/b91b12e0-9fe1-11ec-a634-2f1716dd45af-Take-support.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-09",
+      "description": "4 points to remember\n\nPattern traded:  Taking Support\nNature of patterns:  Bullish\nType of pattern:  Reversal\nFrequency:  Very Often",
+      "exercise_id": [
+        "1",
+        "3",
+        "9"
+      ]
+    }
+  },
+  {
+    "slug": "resistance-taken",
+    "title": "Resistance Taken",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/0e18b140-9fe1-11ec-a634-2f1716dd45af-Take-Resistance.png",
+        "imgix_url": "https://imgix.cosmicjs.com/0e18b140-9fe1-11ec-a634-2f1716dd45af-Take-Resistance.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-09",
+      "description": "4 points to remember\n\nPattern traded: Taking Resistance\nNature of patterns: Bearish\nType of pattern: Reversal\nFrequency: Very Often",
+      "exercise_id": [
+        "1",
+        "3",
+        "10"
+      ]
+    }
+  },
+  {
+    "slug": "support-breakdown",
+    "title": "Support Breakdown",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/99fc6b30-9fe0-11ec-a634-2f1716dd45af-Support-Breakdown.png",
+        "imgix_url": "https://imgix.cosmicjs.com/99fc6b30-9fe0-11ec-a634-2f1716dd45af-Support-Breakdown.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-09",
+      "description": "4 points to remember\n\nPattern traded: Support Breakdown\nNature of patterns: Bearish\nType of pattern: Continuation\nFrequency: Often",
+      "exercise_id": [
+        "3",
+        "11"
+      ]
+    }
+  },
+  {
+    "slug": "role-reversal",
+    "title": "Role Reversal",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/1b496bd0-9fe0-11ec-a634-2f1716dd45af-Role-Reversal.png",
+        "imgix_url": "https://imgix.cosmicjs.com/1b496bd0-9fe0-11ec-a634-2f1716dd45af-Role-Reversal.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-09",
+      "description": "4 points to remember\n\nPattern traded: Role Reversal\nNature of patterns: Depends on the prices giving a breakout or breakdown\nType of pattern: Reversal\nFrequency: Often",
+      "exercise_id": [
+        "12"
+      ]
+    }
+  },
+  {
+    "slug": "support-taken",
+    "title": "Support Taken",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/554d3790-9fdf-11ec-a634-2f1716dd45af-Overall-Support-and-Resistance.png",
+        "imgix_url": "https://imgix.cosmicjs.com/554d3790-9fdf-11ec-a634-2f1716dd45af-Overall-Support-and-Resistance.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-09",
+      "description": "4 points to remember\n\nPattern traded: Taking Support\nNature of patterns: Bullish\nType of pattern: Reversal\nFrequency: Very Often",
+      "exercise_id": [
+        "1",
+        "3",
+        "9"
+      ]
+    }
+  },
+  {
+    "slug": "inside-bar",
+    "title": "Inside Bar",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/a35167a0-9fde-11ec-a634-2f1716dd45af-Inside-Bar.png",
+        "imgix_url": "https://imgix.cosmicjs.com/a35167a0-9fde-11ec-a634-2f1716dd45af-Inside-Bar.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-09",
+      "description": "4 points to remember\n\nPattern traded: Inside Bar\nNature of patterns:  Depends on the on going trend\nType of pattern: Continuation\nFrequency: Often",
+      "exercise_id": [
+        "4"
+      ]
+    }
+  },
+  {
+    "slug": "bullish-flag",
+    "title": "Bullish Flag",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/ef700ed0-9fdd-11ec-a634-2f1716dd45af-Bullish-Flag.png",
+        "imgix_url": "https://imgix.cosmicjs.com/ef700ed0-9fdd-11ec-a634-2f1716dd45af-Bullish-Flag.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-09",
+      "description": "4 points to remember\n\nPattern traded: Bullish Flag\nNature of patterns: Bullish\nType of pattern: Continuation\nFrequency: Not very often",
+      "exercise_id": [
+        "5"
+      ]
+    }
+  },
+  {
+    "slug": "resistance-breakout",
+    "title": "Resistance Breakout",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/cca5eb50-9fdc-11ec-a634-2f1716dd45af-Breaks-Resistance.png",
+        "imgix_url": "https://imgix.cosmicjs.com/cca5eb50-9fdc-11ec-a634-2f1716dd45af-Breaks-Resistance.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-09",
+      "description": "5 points to remember\n\nPattern traded: Resistance Breakout\nNature of patterns: Bullish\nType of pattern: Continuation\nFrequency: Very Often",
+      "exercise_id": [
+        "2",
+        "3"
+      ]
+    }
+  },
+  {
+    "slug": "resistance-band-breakout-trade",
+    "title": "Resistance Band Breakout trade",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/16715380-9fb7-11ec-a634-2f1716dd45af-Breakout-Resistance-Band.png",
+        "imgix_url": "https://imgix.cosmicjs.com/16715380-9fb7-11ec-a634-2f1716dd45af-Breakout-Resistance-Band.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-09",
+      "description": "4 points to remember\n\nPattern traded: Resistance band breakout\nNature of pattern: Bullish\nType of pattern: Continuation\nFrequency: Very Often",
+      "exercise_id": [
+        "8"
+      ]
+    }
+  },
+  {
+    "slug": "major-global-index-returns-in-past-5-years",
+    "title": "Major Global Index returns in past 5 years",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/1274c6f0-9fb6-11ec-a634-2f1716dd45af-2.png",
+        "imgix_url": "https://imgix.cosmicjs.com/1274c6f0-9fb6-11ec-a634-2f1716dd45af-2.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-01",
+      "description": "Mera desh badal raha hai, aage badh raha hai?\nTop 5 countries giving the best returns (index)\n\nUSA\nIndia\nFrance\nJapan\nGermany\n\nWhich index is most likely to yield the most returns in the next 5 years?",
+      "exercise_id": []
+    }
+  },
+  {
+    "slug": "major-stocks-in-52-week-low",
+    "title": "Major Stocks in 52 week low",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/657e3670-9fb5-11ec-a634-2f1716dd45af-1.png",
+        "imgix_url": "https://imgix.cosmicjs.com/657e3670-9fb5-11ec-a634-2f1716dd45af-1.png"
+      },
+      "image2": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-05",
+      "description": "Nifty after making a high of 18,604.45 and Sensex making a high of 62,245.43 have corrected over 10%. Situations like these get investors interested because everyone buys the dip :p\n\nThese are a few stocks currently trading close to their 52-week lows. Good time to buy?",
+      "exercise_id": []
+    }
+  },
+  {
+    "slug": "tata-group-vs-reliance-group",
+    "title": "Tata Group vs Reliance Group",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/33e72a00-9fb4-11ec-a634-2f1716dd45af-tatavreliance.png",
+        "imgix_url": "https://imgix.cosmicjs.com/33e72a00-9fb4-11ec-a634-2f1716dd45af-tatavreliance.png"
+      },
+      "image2": {
+        "url": "https://cdn.cosmicjs.com/44dbd4f0-9fb4-11ec-a634-2f1716dd45af-tatavreliance1.png",
+        "imgix_url": "https://imgix.cosmicjs.com/44dbd4f0-9fb4-11ec-a634-2f1716dd45af-tatavreliance1.png"
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-02-25",
+      "description": "Did you know? Tata Sons (Parent company of all major Tata companies)  is the biggest promoter group in India and Reliance Industries is the company with the highest market capitalisation in India (listed companies)\n\nBoth are major groups and have helped India grow. Don't forget to swipe left and see a development we observed on Mukesh Ambani's Reliance Group",
+      "exercise_id": []
+    }
+  },
+  {
+    "slug": "results-of-top-steel-stocks-for-q3",
+    "title": "Results of Top Steel Stocks for Q3",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/0da14d40-9fb3-11ec-a634-2f1716dd45af-metal1.png",
+        "imgix_url": "https://imgix.cosmicjs.com/0da14d40-9fb3-11ec-a634-2f1716dd45af-metal1.png"
+      },
+      "image2": {
+        "url": "https://cdn.cosmicjs.com/59be2a40-9fb3-11ec-a634-2f1716dd45af-metal2.png",
+        "imgix_url": "https://imgix.cosmicjs.com/59be2a40-9fb3-11ec-a634-2f1716dd45af-metal2.png"
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-02-28",
+      "description": "Results of Top 5 Steel Stocks:\n\nTata Steel\nJSW Steel\nSAIL\nAPL Apollo Tubes\nJindal Stainless\n\nDon't forget to swipe left and check out an opportunity in another hidden metal sector gem",
+      "exercise_id": []
+    }
+  },
+  {
+    "slug": "brands-operated-by-itc",
+    "title": "Brands operated by ITC",
+    "content": "",
+    "metadata": {
+      "image": {
+        "url": "https://cdn.cosmicjs.com/6c6de820-9fb2-11ec-a634-2f1716dd45af-ITC-1.png",
+        "imgix_url": "https://imgix.cosmicjs.com/6c6de820-9fb2-11ec-a634-2f1716dd45af-ITC-1.png"
+      },
+      "image2": {
+        "url": "https://cdn.cosmicjs.com/6fd248d0-9fb2-11ec-a634-2f1716dd45af-ITC2.png",
+        "imgix_url": "https://imgix.cosmicjs.com/6fd248d0-9fb2-11ec-a634-2f1716dd45af-ITC2.png"
+      },
+      "image3": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_4": {
+        "url": null,
+        "imgix_url": null
+      },
+      "image_5": {
+        "url": null,
+        "imgix_url": null
+      },
+      "date": "2022-03-03",
+      "description": "ITC - the meme stock has been focusing a lot on growing its FMCG and Agriculture business. Here are a few brands operated by the tobacco giant.\n\nDon't forget to swipe left and capitalise on an opportunity in ITC",
+      "exercise_id": []
+    }
   }
 ];
 
